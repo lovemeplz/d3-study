fe3d7c941078acbca07c6634aa908d8841cc9e63
shenzq <shenzq@citycloud.com.cn>
2019-11-06 11:26:00 +0800

    feat(barChart1.vue): 完成barChart1 example

--- src/views/basic/barChart1.vue
+++ src/views/basic/barChart1.vue
@@ -1,20 +1,42 @@
 <template>
   <div class="bar-chart1-page">
-    sda
+    <div class="chart">
+    </div>
   </div>
 </template>
 
 <script>
-// import BarChart from './barChart'
-// import ThreeCircles from './threeCircles'
+import * as d3 from  "d3"
+const data = [4, 8, 15, 16, 100, 42];
 export default {
-  // components: {
-  //   BarChart,
-  //   ThreeCircles 
-  // }
+  methods: {
+    renderChart() {
+      // 比例尺  最大即为420 类似定义域值域
+      var x = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([0, 420]);
+        
+      d3.select(".chart")
+      .selectAll("div")
+        .data(data)
+      .enter().append("div")
+        .style("width", function(d) { return x(d) + "px"; })
+        .text(function(d) { return d; });
+    }
+  },
+  mounted() {
+    this.renderChart()
+  }
 }
 </script>
 
 <style>
-
+.chart div {
+  font: 10px sans-serif;
+  background-color: steelblue;
+  text-align: right;
+  padding: 3px;
+  margin: 1px;
+  color: white;
+}
 </style>
